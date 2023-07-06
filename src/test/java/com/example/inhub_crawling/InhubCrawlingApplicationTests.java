077144da884c320c009a352c6ada2b643715baa4
chkim429 <ckdgjs000@naver.com>
2023-07-06 14:43:58 +0900

    초기 세팅

---
+++ src/test/java/com/example/inhub_crawling/InhubCrawlingApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.inhub_crawling;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InhubCrawlingApplicationTests {
+
+    @Test
+    @DisplayName("")
+    void t001() throws Exception {
+    }
+
+}
